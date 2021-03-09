61cc6ab11b9dcdeb8e6c2364a51f5011af99ede4
Hunter MM <hunterkantner@gmail.com>
2021-03-08 17:55:01 -0800

    cleaned php pls work

--- contact-form.php
+++ contact-form.php
@@ -15,67 +15,6 @@ if (array_key_exists('email', $_POST)) {
     $mail->Port = 25;
     $mail->setFrom('avcdszxa@avcdoman.com', 'Admin');
     $mail->addAddress('admin@avcdoman.com');
-    
-    $availability = array();
-    if(
-        isset($_POST['monday1'])
-    ) {
-        array_push($availability, "monday 12-4");
-    }
-    if(
-        isset($_POST['monday2'])
-    ) {
-        array_push($availability, "monday 4-8");
-    }
-    if(
-        isset($_POST['tuesday1'])
-    ) {
-        array_push($availability, "tuesday 12-4");
-    }
-    if(
-        isset($_POST['tuesday2'])
-    ) {
-        array_push($availability, "tuesday 4-8");
-    }
-    if(
-        isset($_POST['wednesday1'])
-    ) {
-        array_push($availability, "wednesday 12-4");
-    }
-    if(
-        isset($_POST['wednesday2'])
-    ) {
-        array_push($availability, "wednesday 4-8");
-    }
-    if(
-        isset($_POST['friday1'])
-    ) {
-        array_push($availability, "friday 12-4");
-    }
-    if(
-        isset($_POST['friday2'])
-    ) {
-        array_push($availability, "friday 4-8");
-    }
-    if(
-        isset($_POST['saturday1'])
-    ) {
-        array_push($availability, "saturday 12-4");
-    }
-    if(
-        isset($_POST['saturday2'])
-    ) {
-        array_push($availability, "saturday 4-8");
-    }
-//    if(array_key_exists('referenceFile', $_FILES)) {
-//     $ext = PHPMailer::mb_pathinfo($_FILES['referenceFile']['name'], PATHINFO_EXTENSION);
-//     $uploadFile = tempnam(sys_get_temp_dir(), hash('sha256', $_FILES['referenceFile']['name'])) . '.' . $ext;
-//    }
-
-    // $name = $_POST['Name'];
-    // $email = $_POST['Email'];
-    // $instagram = $_POST['Instagram'];
-    // $message = $_POST['Message'];
 
     $mail->isHTML(false);
     $mail->Subject = 'New Tattoo Inquiry';
@@ -91,11 +30,8 @@ EOT;
     if (!$mail->send()) {
         echo 'Sorry, something went wrong. Please try again later.';
     } else {
-        echo 'ok';
+        header("Location: https://avcdoman.com/thankyou.html");
     }
-};
-?>
+}
 
-<?php
-header("Location: https://avcdoman.com/thankyou.html");
 ?>
