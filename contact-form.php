c52851b8c2d3e2a2bb62249539d848e5b24ec542
Hunter MM <hunterkantner@gmail.com>
2021-03-08 21:23:24 -0800

    big revert

--- contact-form.php
+++ contact-form.php
@@ -7,34 +7,20 @@ require 'plugins/PHPMailer/src/Exception.php';
 require 'plugins/PHPMailer/src/PHPMailer.php';
 require 'plugins/PHPMailer/src/SMTP.php';
 
-$msg = '';
 
-if (array_key_exists('email', $_POST)) {
+if (isset($_POST['Email'])) {
     date_default_timezone_set('Etc/UTC');
     require '../vendor/autoload.php';
     $mail = new PHPMailer();
-    $mail->setFrom('avcdszxa@avcdoman.com', 'Admin');
-    $mail->addAddress('admin@avcdoman.com');
-
-    if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
-        $mail->Subject = 'New tattoo inquiry';
-        $mail->isHTML(false);
-        $mail->Body = <<<EOT
+    $email_to = "admin@avcdoman.com";
+    $email_subject = "New tattoo inquiry";
+    $mail->isHTML(true);
+    $mail->Body = <<<EOT
 Email: {$_POST['email']}
 Name: {$_POST['name']}
 Message: {$_POST['message']}
 EOT;
-        
-        if (!$mail->send()) {
-            $msg = 'Sorry, something went wrong. Please try again later.';
-        } else {
-            $msg = 'Message sent! Thanks for contacting us.';
-        }
-    } else {
-        $msg = 'Invalid email address, message ignored.';
-    }
-} else {
-    $msg = "what the fuck";
+    $mail->send();
 }
-print "<h2>$msg</h2>"
+header("Location: https://avcdoman.com/thankyou.html");
 ?>
