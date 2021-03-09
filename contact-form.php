6a879a48d3858d96f3eb47c5447d1b42908db540
Hunter MM <hunterkantner@gmail.com>
2021-03-09 10:28:17 -0800

    added availability

--- contact-form.php
+++ contact-form.php
@@ -17,12 +17,67 @@ if (isset($_POST['Email'])) {
     $mail->addAddress("admin@avcdoman.com");
     $mail->addReplyTo($_POST['Email'], $_POST['Name']);
     $mail->Subject = "New tattoo inquiry";
+    $availability = array();
+    if(
+        isset($_POST['monday1'])
+    ) {
+        array_push($availability, "monday 12-4");
+    }
+    if(
+        isset($_POST['monday2'])
+    ) {
+        array_push($availability, "monday 4-8");
+    }
+    if(
+        isset($_POST['tuesday1'])
+    ) {
+        array_push($availability, "tuesday 12-4");
+    }
+    if(
+        isset($_POST['tuesday2'])
+    ) {
+        array_push($availability, "tuesday 4-8");
+    }
+    if(
+        isset($_POST['wednesday1'])
+    ) {
+        array_push($availability, "wednesday 12-4");
+    }
+    if(
+        isset($_POST['wednesday2'])
+    ) {
+        array_push($availability, "wednesday 4-8");
+    }
+    if(
+        isset($_POST['friday1'])
+    ) {
+        array_push($availability, "friday 12-4");
+    }
+    if(
+        isset($_POST['friday2'])
+    ) {
+        array_push($availability, "friday 4-8");
+    }
+    if(
+        isset($_POST['saturday1'])
+    ) {
+        array_push($availability, "saturday 12-4");
+    }
+    if(
+        isset($_POST['saturday2'])
+    ) {
+        array_push($availability, "saturday 4-8");
+    }
+    foreach($availability as $a) { 
+        $b = $a . "<br>";
+    }
     $mail->Body = <<<EOT
 Form details below: <br><br>
 Name:  {$_POST['Name']} <br>
 Email: {$_POST['Email']} <br>
 Instagram: {$_POST['Instagram']} <br>
 Message: {$_POST['Message']} <br>
+Availability: $b
 EOT;
     if (!$mail->send()){
         $msg = 'Mailer Error: ' . $mail->ErrorInfo;
