b836e84acf7140e8bb6cd9540a36a65986c8c278
Hunter MM <hunterkantner@gmail.com>
2021-03-15 16:59:00 -0700

    added console log check

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ function faqDisplay () {
 }
 
 function isCaptchaChecked() {
-   console.log("Captcha checked");
+   console.log(grecaptcha.getResponse());
    return grecaptcha && grecaptcha.getResponse().length !== 0;
  }
 
 function submitDisable () {
    var submitBtn = document.getElementById("Submit");
    submitBtn.removeAttribute("disabled");
+   console.log("button enabled")
 }
 
 if (isCaptchaChecked()) {
