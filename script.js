519a81558de7600c2bd9a637fd47c6104ba4e2e8
Hunter MM <hunterkantner@gmail.com>
2021-03-18 12:14:40 -0700

    removed commented code

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-function faqDisplay () {
-   let element = document.getElementById("FAQ");
-   element.classList.toggle("hidden");
+function faqDisplay() {
+  let element = document.getElementById("FAQ");
+  element.classList.toggle("hidden");
 }
 
-function isCaptchaChecked() {   
-   if (grecaptcha && grecaptcha.getResponse().length !== 0) {
-      var submitBtn = document.getElementById("Submit");
-   submitBtn.removeAttribute("disabled");
-   console.log("button enabled")
-   } else {
-      console.log("error")
-   }
- }
-
-// function submitDisable () {
-//    var submitBtn = document.getElementById("Submit");
-//    submitBtn.removeAttribute("disabled");
-//    console.log("button enabled")
-// }
-
+function isCaptchaChecked() {
+  if (grecaptcha && grecaptcha.getResponse().length !== 0) {
+    var submitBtn = document.getElementById("Submit");
+    submitBtn.removeAttribute("disabled");
+    console.log("button enabled");
+  } else {
+    console.log("error");
+  }
+}
