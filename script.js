cbf84b5a622a1394a7bb3c2faf464cbab67e1061
Hunter MM <hunterkantner@gmail.com>
2021-03-29 16:23:38 -0700

    added IG grid to about page

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ function isCaptchaChecked() {
     console.log("error");
   }
 }
+
+function igData() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var igRes = JSON.parse(this.responseText);
+      for (i = 0; i < 9; i++) {
+        document.getElementById("img" + i).innerHTML =
+          "<a href="+ igRes.data[i].permalink +" ><img src=" + igRes.data[i].media_url + " /></a>";
+      }
+    }
+  };
+  xhttp.open(
+    "GET",
+    "https://graph.instagram.com/me/media?fields=id,media_type,media_url,permalink&access_token=IGQVJXaUgwYXp1cG95QVJvSXhySVNYS1NmRTJzRDNQdXhrbkJHM2V2UGhmc1JjckZACN2o0dHcxSGZA1V3pvTnRHazE2SGxUSVJ1c1FOQTdram1sR1NrOFBUcnVPRVNaS1dvS2F4RE5fQk9hUFppeGVGWQZDZD",
+    true
+  );
+  xhttp.send();
+}
